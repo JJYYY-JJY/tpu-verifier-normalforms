900d1671c0c0f02675d00c4a3fefe79a9f3c5c38
Junye <jijunye1@outlook.com>
2026-04-28 04:03:45 -0700

    feat(certificates): export replay and verification APIs
    - Surface new replay and verification helpers for external use

--- src/nf_agent/certificates/__init__.py
+++ src/nf_agent/certificates/__init__.py
@@ -5,8 +5,11 @@ from nf_agent.certificates.snf_schema import (
     SNF_CERTIFICATE_SCHEMA_VERSION,
     IntegerMatrixOp,
     SNFCertificate,
+    replay_snf_certificate,
     snf_certificate_json_schema,
     validate_snf_certificate_record,
+    verify_snf_certificate,
+    verify_snf_certificate_record,
 )
 
 __all__ = [
@@ -14,6 +17,9 @@ __all__ = [
     "SNF_CERTIFICATE_SCHEMA_VERSION",
     "IntegerMatrixOp",
     "SNFCertificate",
+    "replay_snf_certificate",
     "snf_certificate_json_schema",
     "validate_snf_certificate_record",
+    "verify_snf_certificate",
+    "verify_snf_certificate_record",
 ]
