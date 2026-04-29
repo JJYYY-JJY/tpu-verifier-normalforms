10423ac5bb19e253f83b5962b6de347b5f1bdfb7
Junye <jijunye1@outlook.com>
2026-04-28 17:16:00 -0700

    feat(reports): export report builder from package init
    - Expose report config type and builder at package level for easy import

---
+++ src/nf_agent/reports/__init__.py
@@ -0,0 +1,5 @@
+"""Report generation helpers."""
+
+from nf_agent.reports.benchmark_report import BenchmarkReportConfig, build_benchmark_report
+
+__all__ = ["BenchmarkReportConfig", "build_benchmark_report"]
